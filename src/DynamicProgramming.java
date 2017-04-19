1129ffea2c605998908fd5d832a48304d0796b1f
Brett W <bwilhelm@iastate.edu>
2017-04-19 13:56:21 -0500

    Filled template for DynProg.java and ImProc.java

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+
 /**
  * @author Zach Johnson
  * @author Brett Wilhelm
  */
 
 public class DynamicProgramming {
+	public DynamicProgramming() {
+		
+	}
 	
+	public static ArrayList<Integer> minCostVC(int[][] M) {
+		
+		
+		return null;
+	}
+	
+	public static String stringAlignment(String x, String y) {
+		
+		
+		return null;
+	}
 }
---
+++ src/ImageProcessor.java
@@ -0,0 +1,16 @@
+/**
+ * @author Zach Johnson
+ * @author Brett Wilhelm
+ */
+
+public class ImageProcessor {
+	public ImageProcessor(String imageFile) {
+		
+	}
+	
+	public Picture reduceWidth(double x) {
+		
+		
+		return new Picture(0, 0);
+	}
+}
