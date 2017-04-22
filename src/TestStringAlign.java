a3b61459855f3e1a40ab987b2d17381bcd778d40
Zach <Zachj@iastate.edu>
2017-04-22 17:28:56 -0500

    created testStringAlign

---
+++ src/TestStringAlign.java
@@ -0,0 +1,11 @@
+
+public class TestStringAlign {
+	public static void main(String[] args) {
+		//DynamicProgramming dp = new DynamicProgramming();
+		String s1 = "GCCCTAGCG";
+		String s2 = "GCGCAATG";
+		
+		String test = DynamicProgramming.stringAlignment(s1, s2);
+		System.out.println("expected: GCGC$AATG\n Actual:" + test);
+	}
+}
