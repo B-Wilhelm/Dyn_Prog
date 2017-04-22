8e83e121aca9bd126caaff7a235b64f7b9f9aab1
Brett W <bwilhelm@iastate.edu>
2017-04-22 14:30:52 -0500

    Just uploading whatever changes were made last night

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -4,23 +4,34 @@
  */
 
 public class ImageProcessor {
+	Picture p;
 	
 	/**
-	 * 
-	 * @param imageFile
+	 * Constructor
+	 * @param imageFile Name of the image to be manipulated
 	 */
 	public ImageProcessor(String imageFile) {
-		
+		p = new Picture(0, 0);
 	}
 	
 	/**
-	 * 
-	 * @param x
-	 * @return
+	 * Compresses the width of the image to a smaller (given) dimension
+	 * @param x Width of the file post-modification
+	 * @return The picture after the width has been reduced/modified
 	 */
 	public Picture reduceWidth(double x) {
+		Picture p = new Picture((int)x, 0);
 		
 		
-		return new Picture(0, 0);
+		
+		return p;
+	}
+	
+	private int dist(int[] p, int[] q) {
+		int distance = 0;
+		
+		distance = Math.pow(p[0]-q[0], 2) + Math.pow(p[1]-q[1], 2) + Math.pow(p[2]-q[2], 2);
+		
+		return distance;
 	}
 }
